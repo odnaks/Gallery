1ea62f7cefe47168fa49f7c90d40211f22779e78
odnaks <xvluk@ya.ru>
2020-05-27 14:06:58 +0300

    init

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '13.1'
+
+target 'gallery' do
+  use_frameworks!
+
+  pod 'Alamofire', '~> 5.0.0-rc.3'
+  pod 'Kingfisher', '~> 5.10.1'
+  pod 'SwiftyJSON', '~> 5.0.0'
+
+end
---
+++ gallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gallery
+//
+//  Created by 18450686 on 27.05.2020.
+//  Copyright © 2020 Ksenia Lukoshkina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
