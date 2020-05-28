8ed1004bc3e220810f9796750dbe384a7775edbf
odnaks <xvluk@ya.ru>
2020-05-28 10:59:33 +0300

    refactoring

--- gallery/ViewController.swift
+++ gallery/ViewController.swift
@@ -28,8 +28,6 @@ struct myStr {
 class ViewController: UIViewController {
     @IBOutlet weak var imageScrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
-//    var zoomImage = UIImage(named: "graph")
-    
     
     func getPhotos(completion: @escaping ([myStr]) -> Void){
         let baseUrl = "https://picsum.photos/v2"      //"https://api.vk.com/method"
@@ -38,14 +36,12 @@ class ViewController: UIViewController {
                     "page": "1",
                     "limit": "1"
                 ]
-                // https://api.vk.com/method/friends.get?access_token=1ea08d690aa26a541771c17b304c1be12c79e522e13d0575b0c623c728f8d51679af9ab6f2c65edaa8438&user_id=566604693&v=5.00
                 let url = baseUrl+path
                 
                 AF.request(url, method: .get, parameters: parameters).responseJSON { repsonse in
                     switch repsonse.result {
                         case let .success(data):
                             let json = JSON(data)
-        //                    var users:[User] = []
                             var urls:[myStr] = []
                             for item in json {
                 
@@ -53,13 +49,6 @@ class ViewController: UIViewController {
                                 let url: String = item.1["download_url"].stringValue
                                 let he: Int = item.1["height"].intValue
                                 let wi: Int = item.1["width"].intValue
-//                                self.image1?.kf.setImage(with: URL(string: url))
-//                                self.imageviewM = self.image1
-        //                        let strUrl: String = item.1["photo_200_orig"].stringValue
-        //                        let firstName: String = item.1["first_name"].stringValue
-        //                        let lastName: String = item.1["last_name"].stringValue
-        //                        let id: String = item.1["id"].stringValue
-        //                        users.append(User(photoLink: strUrl, firstName: firstName, lastName: lastName, id: id))
                                 var st = myStr(url: url, wid: wi, hei: he)
         
                                 urls.append(st)
@@ -79,13 +68,11 @@ class ViewController: UIViewController {
         let heightScale = view.frame.height / imageSize.height
         let minScale = min(widthScale, heightScale)
 
-        // Scale the image down to fit in the view
         imageScrollView.minimumZoomScale = minScale
         imageScrollView.zoomScale = minScale
         imageScrollView.delegate = self as? UIScrollViewDelegate
         imageScrollView.bounds = self.view.bounds
         
-        // Set the image frame size after scaling down
         let imageWidth = imageSize.width * minScale
         let imageHeight = imageSize.height * minScale
         let newImageFrame = CGRect(x: 0, y: 0, width: imageWidth, height: imageHeight)
@@ -96,7 +83,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        var urls: [String] = []
         getPhotos{ [weak self] urls in
             print(urls[0])
             let url = urls[0].url
@@ -105,10 +91,6 @@ class ViewController: UIViewController {
             self?.imageView.kf.setImage(with: URL(string: url))
             self?.setMinZoomScaleForImageSize(CGSize(width: width, height: height))
         }
-        
-//        imageviewM = image1
-//        imageView.image = zoomImage
-        // Do any additional setup after loading the view.
     }
 
     
@@ -124,22 +106,6 @@ class ViewController: UIViewController {
         imageScrollView.delegate = self as? UIScrollViewDelegate
     }
         
-//        let widthScale = view.frame.width / imageView.bounds.width
-//        let heightScale = view.frame.height / imageView.bounds.height
-//        let minScale = min(widthScale, heightScale)
-//        imageScrollView.minimumZoomScale = minScale
-        
-//        imageScrollView.contentInset.left = 0
-//        imageScrollView.contentInset.right = 0
-        
-        
-//        let ratio = zoomImage.bounds.
-//        let ratio = zoomImage.size.width / zoomImage.size.height
-//        let newHeight = view.frame.width / ratio
-//        constraintHeight.constant = newHeight
-//        view.layoutIfNeeded()
-//
-    
     
     private func centerImage() {
         print("centerImage")
@@ -152,27 +118,6 @@ class ViewController: UIViewController {
 //        print(horizontalPadding)
         imageScrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
     }
-//
-//    private func zoomRectangle(scale: CGFloat, center: CGPoint) -> CGRect {
-//        var zoomRect = CGRect.zero
-//        zoomRect.size.height = imageView.frame.size.height / scale
-//        zoomRect.size.width  = imageView.frame.size.width  / scale
-//        zoomRect.origin.x = center.x - (center.x * imageScrollView.zoomScale)
-//        zoomRect.origin.y = center.y - (center.y * imageScrollView.zoomScale)
-//
-//        return zoomRect
-//    }
-//
-//    @IBAction func doubleTapImage(_ sender: UITapGestureRecognizer) {
-//
-//        print("taptap")
-//        if imageScrollView.zoomScale == imageScrollView.minimumZoomScale {
-//            imageScrollView.zoom(to: zoomRectangle(scale: imageScrollView.maximumZoomScale, center: sender.location(in: sender.view)), animated: true)
-//        } else {
-//            imageScrollView.setZoomScale(imageScrollView.minimumZoomScale, animated: true)
-//        }
-//    }
-
 }
 
 extension ViewController: UIScrollViewDelegate {
