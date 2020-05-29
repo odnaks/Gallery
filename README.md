2f0f8d404d3c2f40a95bd85ad455205ccffa5a83
odnaks <xvluk@ya.ru>
2020-05-29 12:07:35 +0300

    screen cast

---
+++ README.md
@@ -0,0 +1,3 @@
+### Test task for King Bird Studio
+
+<img scr="https://github.com/odnaks/gallery/1.gif" width="400">
--- gallery/AppDelegate.swift
+++ gallery/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
+    // MARK: UISceneSession Lifecyclega
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
